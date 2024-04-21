73a78baac803b00896e56f1e973b593466dc2a69
TerSimaN <mcsiman998@gmail.com>
2024-04-21 18:07:01 +0300

    Added special offer implementation

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,37 @@
+import Image from "next/image"
+import { arrowRight } from "../app/assets/icons"
+import { offer } from "../app/assets/images"
+import Button from "../components/Button"
+
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className="flex justify-center items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <Image
+          src={offer}
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red">Special</span> Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Embark on a shopping journey that redefines your experience with unbeatable deals. 
+          From premier selections to incredible savings, we offer unparalleled value that sets us apart.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Navigate a realm of possibillities designed to fulfill yuor unique desires, 
+          surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop now" iconURL={arrowRight} />
+          <Button label="Learn more" backgroundColor="bg-white" borderColor="border-slate-gray" textColor="text-slate-gray" />
+        </div>
+      </div>
+    </section>
   )
 }
 
