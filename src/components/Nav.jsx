9b8fc5e7f37e2cfc0712b8cf62da9cb2f3e1aadd
TerSimaN <mcsiman998@gmail.com>
2024-04-21 18:03:08 +0300

    Added nav implementation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,42 @@
+import { headerLogo } from "../app/assets/images";
+import { hamburger } from "../app/assets/icons";
+import { navLinks } from "../constants";
+import Image from "next/image";
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header className="padding-x py-8 absolute z-10 w-full">
+        <nav className="flex justify-between items-center max-container">
+            <a href="/">
+                <Image
+                    src={headerLogo}
+                    alt="Logo"
+                    width={130}
+                    height={29}
+                />
+            </a>
+            <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+                {navLinks.map((item) => (
+                    <li key={item.label}>
+                        <a
+                            href={item.href}
+                            className="font-montserrat leading-normal text-lg text-slate-gray"
+                        >
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <div className="hidden max-lg:block">
+                <Image
+                    src={hamburger}
+                    alt="Hamburger"
+                    width={25}
+                    height={25}
+                />
+            </div>
+        </nav>
+    </header>
   )
 }
 
