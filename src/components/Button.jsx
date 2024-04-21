fbbb9a9a212eca0c3d41a0d78f35945ecd67baa5
TerSimaN <mcsiman998@gmail.com>
2024-04-21 18:02:52 +0300

    Added button implementation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,18 @@
-const Button = () => {
+import Image from "next/image"
+
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) => {
   return (
-    <div>Button</div>
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg 
+      leading-none ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : `bg-coral-red text-white border-coral-red`} 
+      rounded-full ${fullWidth && `w-full`}`}>
+        {label}
+
+        {iconURL && <Image
+            src={iconURL}
+            alt="arrow right icon"
+            className="ml-2 rounded-full w-5 h-5"
+        />}
+    </button>
   )
 }
 
