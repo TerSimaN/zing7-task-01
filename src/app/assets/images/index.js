22b36955c1f81c0d86b9298d1a4a922b933a8acb
TerSimaN <mcsiman998@gmail.com>
2024-04-22 14:17:34 +0300

    Updated

--- src/app/assets/images/index.js
+++ src/app/assets/images/index.js
@@ -16,12 +16,9 @@ import shoe6 from './shoe6.svg'
 import shoe7 from './shoe7.svg'
 import shoe8 from './shoe8.svg'
 
-import thumbnailBackground from './thumbnail-background.svg'
 import thumbnailShoe1 from './thumbnail-shoe1.svg'
 import thumbnailShoe2 from './thumbnail-shoe2.svg'
 import thumbnailShoe3 from './thumbnail-shoe3.svg'
-
-import collectionBackground from './collection-background.svg'
 
 export {
     bigShoe1,
@@ -41,10 +38,7 @@ export {
     shoe7,
     shoe8,
 
-    thumbnailBackground,
     thumbnailShoe1,
     thumbnailShoe2,
     thumbnailShoe3,
-
-    collectionBackground
 }
